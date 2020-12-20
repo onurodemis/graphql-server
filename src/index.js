861c60530e3847fc6a73158d715b74917c8a778f
Onur Odemis <onurodemis@outlook.com>
2020-12-20 20:51:19 +0300

    apollo server is initialized

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('hello');
+import { ApolloServer, gql } from 'apollo-server-express';
+import express from 'express';
+
+const app = express();
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Test'
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () => {
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+})
